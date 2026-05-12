5b8f8c1cbdd116540d9abfa6f8bdffd69b37cf64
Wellington Do Nascimento Silva <wellington.nascimento@ftd.com.br>
2026-05-12 15:13:18 -0300

    test: add pull request secret canary

---
+++ src/secretScanPrCanary.ts
@@ -0,0 +1,5 @@
+// Intentional fake secret used only to validate pull request secret scanning.
+export const secretScanPrCanary = {
+  aws_access_key_id: "AKIAZ9Y8X7W6V5U4T3S2",
+  aws_secret_access_key: "nE7qL4mT9vB2xR6pS1dH8jK3aC5wY0uFzP4gM6rQ",
+};
