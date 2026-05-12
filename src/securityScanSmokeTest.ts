294b81cf0a0647bbb3b402540f7fdf5d727d3ce5
Wellington Do Nascimento Silva <wellington.nascimento@ftd.com.br>
2026-05-12 14:03:47 -0300

    test: remove fake secret canary

--- src/securityScanSmokeTest.ts
+++
@@ -1,5 +0,0 @@
-// Intentional fake token used only to validate the secret scanning pipeline.
-export const secretScanSmokeTestAwsConfig = {
-  aws_access_key_id: "AKIAQ3X7V6B8N9M2P4R5",
-  aws_secret_access_key: "mF9aVq3rT8yP2sD7gH4jK9lQ1wE5rY6uI0oPzXcB",
-};
